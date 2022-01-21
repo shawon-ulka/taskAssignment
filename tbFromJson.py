d199453bd21b922bf36a230af67675477d53c5bd
shawon <md.shawon.islam@ulkasemi.com>
2022-01-21 11:22:40 +0600

    test bench generation done

---
+++ tbFromJson.py
@@ -0,0 +1,34 @@
+import json
+
+with open("portDataNew","r") as f:
+    allInfoDict=json.load(f)
+f.close()
+def sizeString(size):
+    if size==1:
+        return ""
+    sizeString=f"[{size-1}:0]"
+    return sizeString
+with open("testBench.sv","w") as f:
+    f.writelines("//TestBench Generation\n\n")
+    f.writelines(f"module tb_{allInfoDict['module name']};//TestBench code start\n\n")
+    allPortInfo=allInfoDict["allPortInfo"]
+    index=1
+    for key,value in allPortInfo.items():
+        portInfo=allPortInfo[key]
+        outputLine=""
+        if portInfo["type"]=="wire" and portInfo["direction"]=="input":
+            size=sizeString(portInfo['size'])
+            outputLine=f"\treg {size} {key};\n"
+        elif portInfo["type"]=="reg" and portInfo["direction"]=="output":
+            size=sizeString(portInfo['size'])
+            outputLine=f"\twire {size} {key};\n"
+        elif portInfo["type"]=="wire" and portInfo["direction"]=="inout":
+            size=sizeString(portInfo['size'])
+            outputLine=f"\treg {size} {key};\n"
+        if index==len(allPortInfo):
+            outputLine=outputLine.replace(",","")
+        f.writelines(outputLine)
+        index+=1
+    f.writelines("\nendmodule//TestBench code end")
+
+f.close()
---
+++ testBench.sv
@@ -0,0 +1,14 @@
+//TestBench Generation
+
+module tb_memory;//TestBench code start
+
+	reg  clk;
+	reg [7:0] address;
+	reg [7:0] write_data;
+	reg  cs;
+	reg  we;
+	reg  oe;
+	wire [7:0] read_data;
+	reg  IO_pin;
+
+endmodule//TestBench code end
